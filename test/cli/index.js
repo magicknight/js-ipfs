ce3e2564dd4b499a2b0c2118d1d49fce1d7551df
Michael Garvin <gar+gh@danger.computer>
2018-06-28 11:19:05 -0700

    chore(tests): share ipfs instances between tests
    It accounts for the tests being run from either the cli/index.js
    file, or the individual files themselves, programmatically determining
    whether or not it is responsible for shutting down the instance or
    cleaning up its repoPath
    
    This speeds up tests considerably, from what I can tell testing
    locally on machine.

--- test/cli/index.js
+++ test/cli/index.js
@@ -2,8 +2,38 @@
 'use strict'
 
 const fs = require('fs')
+const spawn = require('../utils/on-and-off')
+const parallel = require('async/parallel')
 
 describe('cli', () => {
+  let daemonOff
+  let daemonOn
+  let ipfsd
+  before(function (done) {
+    // CI takes longer to instantiate the daemon,
+    // so we need to increase the timeout for the
+    // before step
+    this.timeout(60 * 1000)
+    parallel([
+      (cb) => spawn.daemonOff((isNew, ipfs) => {
+        daemonOff = ipfs
+        cb()
+      }),
+      (cb) => spawn.daemonOn((isNew, ipfs, node) => {
+        daemonOn = ipfs
+        ipfsd = node
+        cb()
+      })
+    ], done)
+  })
+
+  after(function (done) {
+    this.timeout(20 * 1000)
+    spawn.cleanDaemon(daemonOff)
+    spawn.stopDaemon(ipfsd, done)
+  })
+
+  //require('./id.js')
   fs.readdirSync(__dirname)
     .filter((file) => file !== 'index.js')
     .forEach((file) => require('./' + file))
